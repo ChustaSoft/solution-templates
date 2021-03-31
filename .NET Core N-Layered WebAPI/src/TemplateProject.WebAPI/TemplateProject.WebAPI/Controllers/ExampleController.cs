08e76fa8a438c8dbefb7e0167843fe3ce36817e0
Xavi Abelaira Rueda <xavi_abelaira@outlook.com>
2021-03-31 18:31:31 +0200

    Updated project with ChustaSoft Common references

---
+++ .NET Core N-Layered WebAPI/src/TemplateProject.WebAPI/TemplateProject.WebAPI/Controllers/ExampleController.cs
@@ -0,0 +1,18 @@
+﻿using ChustaSoft.Common.Base;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TemplateProject.WebAPI.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ExampleController : ApiControllerBase<ExampleController>
+    {
+        public ExampleController(ILogger<ExampleController> logger)
+           : base(logger)
+        {
+            
+        }
+    }
+}
