065b7225b6765bfc9198fa7726e0abbaf0510dac
Xavi Abelaira Rueda <xavi_abelaira@outlook.com>
2021-04-01 16:57:49 +0200

    Template structure created

---
+++ .NET Core N-Layered WebAPI/template/TemplateProject.UnitTests/UnitTestExample.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace $safeprojectname$
+{
+    public class UnitTestExample
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ .NET Core N-Layered WebAPI/template/TemplateProject.WebAPI/Controllers/ExampleController.cs
@@ -0,0 +1,18 @@
+﻿using ChustaSoft.Common.Base;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace $safeprojectname$.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ExampleController : ApiControllerBase<ExampleController>
+    {
+        public ExampleController(ILogger<ExampleController> logger)
+           : base(logger)
+        {
+            
+        }
+    }
+}
